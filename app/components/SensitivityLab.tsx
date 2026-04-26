ded3f299f7a9ea82ff1461c964d4d8f3e1e79761
UnderMild <worautai@gmail.com>
2026-04-26 23:58:45 +0700

    vercel: fix deployment

--- app/components/SensitivityLab.tsx
+++ app/components/SensitivityLab.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { Inputs } from "../lib/types";
 import { runSensitivityAnalysis, SensitivityParam, SensitivityKPI } from "../lib/sensitivity";
 import { formatNumber } from "../lib/format";
